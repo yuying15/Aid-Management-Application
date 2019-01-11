f757df07f613c898700089d728c6a1fab57cf18e
yuying15 <qfu2007@gmail.com>
2019-01-10 22:34:33 -0500

    Update Date.cpp

--- Date.cpp
+++ Date.cpp
@@ -1,20 +1,3 @@
-// Final Project - Milestone 5
-// Name: Qirong Fu
-// Student #: 158899179
-// Course: OOP244 Fall 2018
-// File: Date.cpp
-
-// Final Project Milestone 1
-//
-// Version 1.0
-// Date
-// Author
-// Description
-//
-// Revision History
-// -----------------------------------------------------------
-// Name               Date                 Reason
-/////////////////////////////////////////////////////////////////
 #include <iostream>
 #include <iomanip>
 #include "Date.h"
