90200cc5ed3d85be554f6e2a5fe0124642c2ef9a
yuying15 <qfu2007@gmail.com>
2019-01-10 22:39:45 -0500

    Update Perishable.h

--- Perishable.h
+++ Perishable.h
@@ -1,9 +1,3 @@
-// Final Project - Milestone 5
-// Name: Qirong Fu
-// Student #: 158899179
-// Course: OOP244 Fall 2018
-// File: Perishable.h
-
 #ifndef AID_PERISHABLE_H
 #define AID_PERISHABLE_H
 
